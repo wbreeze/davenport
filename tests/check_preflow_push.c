cb9cf7c0f915f6e1a26dfd220fe75709a2fa916f
Douglas Lovell <doug@wbreeze.com>
2019-04-09 17:13:24 -0300

    Add tests for preflow_push

--- tests/check_preflow_push.c
+++ tests/check_preflow_push.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include "check_davenport.h"
 #include "../src/preflow_push.c"
 
@@ -22,6 +23,59 @@ START_TEST(check_push_excess)
 }
 END_TEST
 
+START_TEST(check_push_capacity)
+{
+  int node_ct = 2;
+  int excess[2];
+  int flow[4];
+  int capacity[4];
+  int send = 4;
+  int cap = 2;
+  int from = 0;
+  int to = 1;
+
+  excess[from] = send;
+  capacity[RCI(from,to,node_ct)] = cap;
+  flow[RCI(from,to,node_ct)] = 0;
+  push(capacity, flow, excess, node_ct, from, to);
+  ck_assert_int_eq(send - cap, excess[from]);
+  ck_assert_int_eq(cap, excess[to]);
+  ck_assert_int_eq(cap, flow[RCI(from,to,node_ct)]);
+  ck_assert_int_eq(-cap, flow[RCI(to,from,node_ct)]);
+}
+END_TEST
+
+START_TEST(check_push_residual)
+{
+  int node_ct = 2;
+  int excess[2];
+  int flow[4];
+  int capacity[4];
+  int send = 4;
+  int cap = 4;
+  int existing = 2;
+  int residual = cap - existing;
+  int from = 0;
+  int to = 1;
+
+  excess[from] = send;
+  capacity[RCI(from,to,node_ct)] = cap;
+  flow[RCI(from,to,node_ct)] = existing;
+  push(capacity, flow, excess, node_ct, from, to);
+  ck_assert_int_eq(send - residual, excess[from]);
+  ck_assert_int_eq(residual, excess[to]);
+  printf("FLOW:\n");
+  for(int i = 0; i < node_ct; ++i) {
+    for (int j = 0; j < node_ct; ++j) {
+      printf("%d\t", flow[RCI(i,j,node_ct)]);
+    }
+    printf("\n");
+  }
+  ck_assert_int_eq(cap, flow[RCI(from,to,node_ct)]);
+  ck_assert_int_eq(-residual, flow[RCI(to,from,node_ct)]);
+}
+END_TEST
+
 Suite *make_preflow_push_suite(void)
 {
   Suite *s;
@@ -32,5 +86,7 @@ Suite *make_preflow_push_suite(void)
 
   suite_add_tcase (s, tc);
   tcase_add_test(tc, check_push_excess);
+  tcase_add_test(tc, check_push_capacity);
+  tcase_add_test(tc, check_push_residual);
   return s;
 }
