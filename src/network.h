22158f1ba1091831683311d61682673362f1da58
Douglas Lovell <doug@wbreeze.com>
2019-04-16 14:41:17 -0300

    Remove type cast from calloc of edge and node arrays

--- src/network.h
+++ src/network.h
@@ -5,8 +5,8 @@
 #define RCI(row, col, node_ct) ((row) * (node_ct) + (col))
 #define ECT(node_ct) ((node_ct) * (node_ct))
 
-#define edge_array_calloc(node_ct) (int *)calloc(ECT(node_ct), sizeof(int))
-#define node_array_calloc(node_ct) (int *)calloc(node_ct, sizeof(int))
+#define edge_array_calloc(node_ct) calloc(ECT(node_ct), sizeof(int))
+#define node_array_calloc(node_ct) calloc(node_ct, sizeof(int))
 
 void edge_array_print(const int *edges, int node_ct);
 void edge_array_printl(const int *edges, int node_ct, char *label);
