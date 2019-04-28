52c1b180517bea3af18007d4168ec545b5c9cb99
Douglas Lovell <doug@wbreeze.com>
2019-04-28 19:44:12 -0300

    Add small, no cycle test network

--- test/test_helper_nets.c
+++ test/test_helper_nets.c
@@ -1,6 +1,17 @@
 #include <assert.h>
 #include "../src/network.h"
 
+void set_majority_net_no_cycles(int *majority, int node_ct)
+{
+  assert(4 <= node_ct);
+  majority[RCI(0,1,node_ct)] = 2;
+  majority[RCI(0,2,node_ct)] = 3;
+  majority[RCI(0,3,node_ct)] = 3;
+  majority[RCI(1,2,node_ct)] = 2;
+  majority[RCI(1,3,node_ct)] = 1;
+  majority[RCI(2,3,node_ct)] = 3;
+}
+
 void set_majority_net_one_cycle_embedded(int *majority, int node_ct)
 {
   assert(5 <= node_ct);
--- test/test_helper_nets.h
+++ test/test_helper_nets.h
@@ -1,6 +1,7 @@
 #ifndef TEST_HELPER_NETS_H
 #define TEST_HELPER_NETS_H
 
+void set_majority_net_no_cycles(int *majority, int node_ct);
 void set_majority_net_one_cycle_embedded(int *majority, int node_ct);
 void set_majority_net_multi_cycle_embedded(int *majority, int node_ct);
 void set_majority_net_two_embedded_cycles(int *majority, int node_ct);
