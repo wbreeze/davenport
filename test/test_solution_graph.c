bc6d9552d7b249f3f18d0316a0d73b88be440905
Douglas Lovell <doug@wbreeze.com>
2019-05-01 21:39:35 -0300

    Assert true and false on has_edge tests

--- test/test_solution_graph.c
+++ test/test_solution_graph.c
@@ -18,9 +18,9 @@ void test_solution_graph_create(void)
   cut_assert_not_null(sol);
 
   cut_assert_equal_int(node_ct, sol->node_ct);
-  cut_assert_equal_int(0, sol->disagreement_count);
+  cut_assert_false(sol->disagreement_count);
   clear_solution_array(sol->solution, node_ct);
-  cut_assert_equal_int(0, sol->set_point);
+  cut_assert_false(sol->set_point);
   clear_edge_stack(sol->edge_stack, node_ct);
 
   sol = solution_graph_destroy(sol);
@@ -33,27 +33,27 @@ void test_solution_graph_add_edge(void)
   SolutionGraph *sol = solution_graph_create(node_ct);
 
   solution_graph_add_edge(sol, 0, 4);
-  cut_assert_equal_int(1, solution_graph_has_edge(sol, 0, 4));
-  cut_assert_equal_int(0, solution_graph_has_edge(sol, 0 ,1));
-  cut_assert_equal_int(0, solution_graph_has_edge(sol, 0 ,3));
+  cut_assert_true(solution_graph_has_edge(sol, 0, 4));
+  cut_assert_false(solution_graph_has_edge(sol, 0 ,1));
+  cut_assert_false(solution_graph_has_edge(sol, 0 ,3));
 
   solution_graph_add_edge(sol, 1, 3);
-  cut_assert_equal_int(1, solution_graph_has_edge(sol, 1 ,3));
-  cut_assert_equal_int(0, solution_graph_has_edge(sol, 1 ,4));
-  cut_assert_equal_int(0, solution_graph_has_edge(sol, 0 ,1));
-  cut_assert_equal_int(0, solution_graph_has_edge(sol, 0 ,3));
+  cut_assert_true(solution_graph_has_edge(sol, 1 ,3));
+  cut_assert_false(solution_graph_has_edge(sol, 1 ,4));
+  cut_assert_false(solution_graph_has_edge(sol, 0 ,1));
+  cut_assert_false(solution_graph_has_edge(sol, 0 ,3));
 
   solution_graph_add_edge(sol, 0, 1);
-  cut_assert_equal_int(1, solution_graph_has_edge(sol, 0 ,1));
-  cut_assert_equal_int(1, solution_graph_has_edge(sol, 0 ,3));
-  cut_assert_equal_int(0, solution_graph_has_edge(sol, 1 ,4));
+  cut_assert_true(solution_graph_has_edge(sol, 0 ,1));
+  cut_assert_true(solution_graph_has_edge(sol, 0 ,3));
+  cut_assert_false(solution_graph_has_edge(sol, 1 ,4));
 
   solution_graph_add_edge(sol, 3, 2);
-  cut_assert_equal_int(1, solution_graph_has_edge(sol, 3 ,2));
-  cut_assert_equal_int(1, solution_graph_has_edge(sol, 0 ,2));
-  cut_assert_equal_int(1, solution_graph_has_edge(sol, 1 ,2));
-  cut_assert_equal_int(0, solution_graph_has_edge(sol, 1 ,4));
-  cut_assert_equal_int(0, solution_graph_has_edge(sol, 4 ,2));
+  cut_assert_true(solution_graph_has_edge(sol, 3 ,2));
+  cut_assert_true(solution_graph_has_edge(sol, 0 ,2));
+  cut_assert_true(solution_graph_has_edge(sol, 1 ,2));
+  cut_assert_false(solution_graph_has_edge(sol, 1 ,4));
+  cut_assert_false(solution_graph_has_edge(sol, 4 ,2));
 
   sol = solution_graph_destroy(sol);
 }
